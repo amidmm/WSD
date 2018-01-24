c53d2201ff4014a53b635f9b0f3c0bec00cbb2f3
Amid Moeinzadeh <Amid.Moeinzadeh@gmail.com>
2018-01-24 22:07:19 +0330

    Change the main

--- Main.py
+++ Main.py
@@ -1,6 +1,19 @@
 from Importer import *
-from DFSpaths import *
+from Navigli import *
+import nltk
+import networkx as nx
+# import matplotlib as plt
+#
+# plt.interactive(False)
+# g = importFromPaj("C:\\Users\\SAMM\\Downloads\\Telegram Desktop\\synset_relation.paj")
+g = importFromNet("C:\\Users\\SAMM\\Desktop\\wordnet3.net")
 
-g = importEFromPaj("C:\\Users\\SAMM\\Downloads\\Telegram Desktop\\synset_relation.paj")
-for i in DFSpaths(g,"13260","1"):
-    print(i)
+sen = ["3131","24874"]
+
+# nx.draw_networkx(g,with_labels=False,node_size=30)
+h = buildGraph(set(sen),g)
+print(len(h))
+# nx.draw(h)
+print(h.node)
+pass
+# print(h.adj)
