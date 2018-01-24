34941b141cc763217f0ee809552c6a69c9716231
hrezaei <mr.hrezaei@gmail.com>
2018-01-24 23:10:49 +0330

    A few bugs fixed in NetworkX API calls

--- Navigli.py
+++ Navigli.py
@@ -5,19 +5,20 @@ def buildGraph(syns,graph):
     g = nx.Graph()
     toverify = []
     for id in syns:
-        g.add_node(id,attr_dict=graph.node[id])
-        toverify.append(id)
+        if id in graph.nodes:
+            g.add_node(id,attr_dict=graph.node[id])
+            toverify.append(id)
     while toverify:
         start = toverify.pop()
         for path in DFSpaths.DFSpaths(graph, start, syns):
             for id in path:
                 if id not in syns and id != start:
                     g.add_node(id,attr_dict=graph.node[id])
-                    syns.add(id)
+                    syns.append(id)
                     # toverify.append(id)
             for (src, dst) in zip(path, path[1:]):
                 try:
-                    g.add_edge(src, dst, attr_dict=graph.edge[src][dst])
+                    g.add_edge(src, dst, attr_dict=graph.edges[(src, dst)])
                 except:
                     pass
     return g
